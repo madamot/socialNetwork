0e859c94ecd381ceb81ef0f25ba7f5621eb34fa7
Madamot <adam@madamot.com>
2019-06-06 00:19:34 +0100

    initial commit

---
+++ functions.php
@@ -0,0 +1,56 @@
+<?php
+
+  $servername = "localhost";
+  $dBUsername = "root";
+  $dBPassword = "root";
+  $dBName = "Madamotbook";
+  $appname = "Madamotbook";
+
+  $connection = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+
+  if (!$connection) {
+    die("Connection failed: ".mysqli_connect_error());
+  }
+
+  function createTable($name, $query) {
+    queryMysql("CREATE TABLE IF NOT EXISTS $name($query)");
+    echo "Table '$name' created or already exists.<br>";
+  }
+
+  function queryMysql($query) {
+    global $connection;
+    $result = $connection->query($query);
+    if (!$result) die($connection->error);
+    return $result;
+  }
+
+  function destroySession() {
+    $_SESSION=array();
+
+    if (session_id() != "" || isset($_COOKIE[session_name()]))
+      setcookie(session_name(), '', time()-2592000, '/');
+
+    session_destroy();
+  }
+
+  function sanitizeString($var) {
+    global $connection;
+    $var = strip_tags($var);
+    $var = htmlentities($var);
+    $var = stripslashes($var);
+    return $connection->real_escape_string($var);
+  }
+
+  function showProfile($user) {
+    if (file_exists("$user.jpg"))
+      echo "<img src='$user.jpg' style='float:left;'>";
+
+    $result = queryMysql("SELECT * FROM profiles WHERE user='$user'");
+
+    if ($result->num_rows) {
+      $row = $result->fetch_array(MYSQLI_ASSOC);
+      echo stripslashes($row['text']) . "<br style='clear:left;'><br>";
+    }
+  }
+
+ ?>
