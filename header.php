b79a950cc1033948c72f44a9ed2ea7f9809d55d6
Madamot <adam@madamot.com>
2019-06-08 17:35:30 +0100

    file corrections upon review

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
   }
   else $loggedin = FALSE;
 
-  echo "<title>$appname$userstr</title><link rel='stylesheet' href='styles.css' type='text/css'" .
+  echo "<title>$appname$userstr</title><link rel='stylesheet' href='style.css' type='text/css'" .
        "</head><body><center><canvas id='logo' width='624' height='96'>$appname</canvas></center>" .
        "<div class='appname'>$appname$userstr</div><script src='javascript.js'></script>";
   if ($loggedin) {
